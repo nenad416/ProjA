a006ae0abdc36c0bebbb7e15f0ba2ed5d28a68a8
Nenad R <nenad@rakitagroup.com>
2017-11-02 15:06:00 -0400

    fix1

--- hello.java
+++ hello.java
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 public class hello{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++) {
-			System.out.println("First Java Count...."+i);
+			System.out.println("First Java CountA...."+i);
 		}
 	}
-	
-=======
-public class hello{
-	public static void main(String[] args){
-		for(int i=1;i<=10;i++) {
-			System.out.println("First Java Count..."+i);
-		}
-	}
-	
->>>>>>> 3ac25dd6f57397abccc1aabaf6ee774e90d98e22
 }
